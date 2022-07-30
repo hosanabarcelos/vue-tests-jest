3d0dbacd508103a91833d27e1bfa352e838a1bbe
Hosana <hosanabarceloscandido@gmail.com>
2022-07-30 14:15:21 -0300

    starting project of tests with vue

---
+++ src/tests/unit/Lance.spec.js
@@ -0,0 +1,22 @@
+import Lance from '@/components/Lance';
+import { mount } from '@vue/test-utils';
+
+test('Não aceita lance com valor menor que zero', () => {
+    const wrapper = mount(Lance); //Monta componente
+    const input = wrapper.find('input'); //Localiza input dentro do componente
+    input.setValue(-100); //Define valor invalido para input
+
+    const lancesEmitidos = wrapper.emitted('novo-lance'); //Escuta evento novo-lance
+    wrapper.trigger('submit'); //Ativa submição do formulário
+    expect(lancesEmitidos).toBeUndefined(); //Espera que lancesEmitidos sejam undefined
+});
+
+test('Emite um lance quando o valor é maior que zero', () => {
+    const wrapper = mount(Lance); //Monta componente
+    const input = wrapper.find('input'); //Localiza input dentro do componente
+    input.setValue(100); //Define valor válido para input
+
+    wrapper.trigger('submit'); //Ativa submição do formulário
+    const lancesEmitidos = wrapper.emitted('novo-lance'); //Escuta evento novo-lance
+    expect(lancesEmitidos).toHaveLength(1); //Espera que lancesEmitidos existam (1 evento lá dentro)
+});
